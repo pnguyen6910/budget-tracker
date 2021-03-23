33ae4e065f50aa676ba01b47a70524d9bea5319f
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-03-23 18:15:35 -0500

    redid activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,42 +1,45 @@
+const CACHE_NAME = 'static-cache-v2'
+const DATA_CACHE_NAME = 'data-cache-v1'
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
+    '/manifest.webmanifest',
     '/style.css',
     '/index.js'
 ]
 
-const PRECACHE = 'precache-v1'
-const RUNTIME = 'runtime'
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
-            .open(PRECACHE)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .open(DATA_CACHE_NAME)
+            .then((cache) => cache.addAll("/api/transaction"))
             .then(self.skipWaiting())
+    )
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
     )
 })
 
 self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
         caches
             .keys()
             .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-            })
-            .then((cachesToDelete) => {
                 return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
+                    cacheNames.map(cache => {
+                        if (cache !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                            console.log("Old cache data removed", cache);
+                            return caches.delete(cache);
+                        }
+                    }))
             })
-            .then(() => self.clients.claim())
-    );
-});
+    )
+})
 
 self.addEventListener('fetch', (event) => {
     if (event.request.url.startsWith(self.location.origin)) {
