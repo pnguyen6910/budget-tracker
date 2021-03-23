ecab81832e0db33a9d15bc74fa2f30a4fbd3f150
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-03-23 18:23:33 -0500

    service worker updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const DATA_CACHE_NAME = 'data-cache-v1'
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/index.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/manifest.webmanifest',
